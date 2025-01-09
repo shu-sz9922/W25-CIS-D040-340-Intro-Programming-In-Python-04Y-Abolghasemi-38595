463665833c8a7798f59dca1b8ed4521db4e0e371
Shu <93468573+shu-sz9922@users.noreply.github.com>
2025-01-08 16:38:26 -0800

    Add files via upload

---
+++ CW/CIS340 - Chapter 1 Exercise 2 - Shu Zhu.py
@@ -0,0 +1,36 @@
+## CIS340 - Chapter 1 Exercise 2 - Shu Zhu
+# Sample Program that demonstrates the print function
+# 
+# The following lines prints 7
+print(3+4)
+#Print Hello World! on two lines
+print("Hello")
+print("World!")
+print("\n")
+# Print multiple values with a single print function call
+print("My favorite number are", 3+4, "and",3+10)
+print("This is the first line \n Now the second line!")
+
+print("Goodbye")
+print()
+print("Hope to see you again")
+
+'''
+#The Output
+
+7
+Hello
+World!
+
+
+My favorite number are 7 and 13
+This is the first line 
+ Now the second line!
+Goodbye
+
+Hope to see you again
+
+
+** Process exited - Return Code: 0 **
+Press Enter to exit terminal
+'''

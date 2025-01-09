cdc872d92380be430524cbd1cdd59171159c8732
Shu <93468573+shu-sz9922@users.noreply.github.com>
2025-01-08 16:20:01 -0800

    Add files via upload

---
+++ CW/CIS40 - Chapter 1 Exercise 1 - Shu.py
@@ -0,0 +1,17 @@
+#"CIS40 - Chapter 1 Exercise 1 - Shu Zhu
+#"This is my first program in Python!"
+age = 1
+print("Age=", age)
+age = age + 1
+print("Age=", age)
+print("Done!")
+
+'''
+Age= 1
+Age= 2
+Done!
+
+
+** Process exited - Return Code: 0 **
+Press Enter to exit terminal
+'''
---
+++ CW/CIS40 CW1 01_08_2025.py
@@ -0,0 +1,19 @@
+#"CIS40 - Chapter 1 Exercise 1 - Shu Zhu
+#"This is my first program in Python!"
+
+'''
+1/8/2025    CIS 340 
+Name: Shu
+Description: This is the first class work trying to print the output
+
+#Comment should be at the begining of the code
+
+Design an application to calculate the pay for an employee 
+based on the rate and the hours
+'''
+
+print("This is my first Python code :D")
+print("This is Shu :)")
+
+print("The answer is ", 1+2)
+print(1+2)

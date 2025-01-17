cdfa42e38b92779a9c6b97d2c06b4ba8f336c27b
Shu <93468573+shu-sz9922@users.noreply.github.com>
2025-01-16 18:23:25 -0800

    Update Correction.py

--- Notes/Correction.py
+++ Notes/Correction.py
@@ -1,21 +1,21 @@
+'''
 Lab Day Quiz
 Due Jan 16 at 11:59pm
 Consider the following code segment:
-
+'''
  
-'''python
 
 a = input("Enter the value of a: ")
 
 b = input("Enter the value of b: ")
 
 print(a + b)
-'''
+
  
 
-When this code segment is run the user enters 1 at the first prompt and 3 at the second prompt. The output displayed is:
+# When this code segment is run the user enters 1 at the first prompt and 3 at the second prompt. The output displayed is:
 
-4
-13
-1+3
-1 + 3
+# 4
+# 13
+# 1+3
+# 1 + 3

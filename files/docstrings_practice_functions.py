1e756710fd0e83d39ec87c62a62a89aa1d12a19c
Shu <93468573+shu-sz9922@users.noreply.github.com>
2025-02-05 15:38:40 -0800

    Add files via upload

---
+++ files/docstrings_practice_functions.py
@@ -0,0 +1,13 @@
+def test():
+    '''
+    The test docstring for the function test.
+    '''
+    print(18)
+
+help(test)
+print('#############################')
+print(test.__doc__)
+print('#############################')
+
+
+        

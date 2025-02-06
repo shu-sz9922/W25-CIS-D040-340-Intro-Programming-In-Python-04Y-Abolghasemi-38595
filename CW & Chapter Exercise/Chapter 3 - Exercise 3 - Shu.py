d6326e7442575d4da2bc5f2c7de067b5c4784330
Shu <93468573+shu-sz9922@users.noreply.github.com>
2025-02-05 16:21:05 -0800

    1

--- CW & Chapter Exercise/Chapter 3 - Exercise 3 - Shu.py
+++ CW & Chapter Exercise/Chapter 3 - Exercise 3 - Shu.py
@@ -1,2 +1,6 @@
-'''   
+'''
+CIS 340 - Shu Zhu
+Rewrite the pay program using try and except 
+so that the program handles non-numeric input gracefully. 
+Also, use the "isdigit" method (assume the user enters non-float numbers) 
 '''

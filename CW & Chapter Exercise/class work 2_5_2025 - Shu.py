b05f7c92eaddce5aca6febc60dc83a59fb46d2f5
Shu <93468573+shu-sz9922@users.noreply.github.com>
2025-02-06 00:25:39 -0800

    class work 2_5_2025

---
+++ CW & Chapter Exercise/class work 2_5_2025 - Shu.py
@@ -0,0 +1,115 @@
+'''
+CIS 340 - class work 2_5_2025 - Shu Zhu
+'''
+#Repeated Steps
+# n = 5
+# while n > 0 :
+#     print(n)
+#     n = n - 1
+# print('Blastoff!')
+# print(n)
+
+'''
+5
+4
+3
+2
+1
+Blastoff!
+0
+'''
+
+
+# #Infinite, never stop due to lack of an updater
+# n = 5
+# while n > 0 :
+#     print('Lather')
+#     print('Rinse')
+# print('Dry off!')
+
+# #do nothing
+# n = 0
+# while n > 0 :
+#     print('Lather')
+#     print('Rinse')
+# print('Dry off!')
+
+
+# #break
+# while True:
+#     line = input('> ')
+#     if line == 'done' :
+#         break
+#     print(line)
+# print('Done!')
+
+'''
+> Hello
+Hello
+> True?
+True?
+> done?
+done?
+> Done
+Done
+> done
+Done!
+'''
+
+
+
+
+# balance = 10.0
+# target = 100.0
+# year = 0
+# rate = 0.025
+# while balance < TARGET :
+#    year = year + 1
+#    interest = balance * RATE/100
+#    balance = balance + interest
+
+# balance = 10.0
+# target = 10.1
+# year = 0
+# rate = 0.03
+# while balance < target :
+#    year = year + 1
+#    interest = balance * rate/100
+#    balance = balance + interest
+#    print(balance, "")
+'''= RESTART: E:/OneDrive - Foothill DeAnza CCD/##25Win/CIS 340/CIS 340 - class work 2_5_2025 - Shu Zhu.py
+10.003 
+10.0060009 
+10.00900270027 
+10.012005401080081 
+10.015009002700406 
+10.018013505401216 
+10.021018909452836 
+10.024025215125672 
+10.02703242269021 
+10.030040532417017 
+10.033049544576741 
+10.036059459440114 
+10.039070277277947 
+10.04208199836113 
+10.045094622960638 
+10.048108151347527 
+10.051122583792932 
+10.054137920568069 
+10.05715416194424 
+10.060171308192823 
+10.06318935958528 
+10.066208316393157 
+10.069228178888075 
+10.072248947341741 
+10.075270622025943 
+10.078293203212551 
+10.081316691173514 
+10.084341086180865 
+10.08736638850672 
+10.090392598423271 
+10.0934197162028 
+10.09644774211766 
+10.099476676440295 
+10.102506519443228 '''
+
